3aa30d47a39eeb140a74e8cf0b8facc1043f36f3
Gilardino <naufal.gilardino@gmail.com>
2024-12-07 09:16:16 +0700

    Result = Num1 * Num2;

--- MainProgram.c
+++ MainProgram.c
@@ -9,8 +9,21 @@ int main ()
     printf("2. Subtraction\n");
     printf("3. Multiplication\n");
     printf("4. Division\n");
-    printf("Select (1-4): ");
-    scanf("%d", &choice);
-
+    switch (choice)
+    {
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        Result = Num1 * Num2;
+        break;
+        case 4:
+        break;
+        default:
+        printf("Select (1-4): ");
+        scanf("%d", &choice);
+        break;
+    }
     return 0;
 }
