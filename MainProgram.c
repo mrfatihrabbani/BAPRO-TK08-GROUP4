9b6f24732f13bcf291d26fd43f6534f57809bb0a
hanifbambang <hanifbambang30@gmail.com>
2024-12-07 08:55:28 +0700

    Create Main Program

---
+++ MainProgram.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    float Num1, Num2, Result;
+    int choice;
+    printf("Calculator by Group 4\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Select (1-4): ");
+    scanf("%d", &choice);
+
+    return 0;
+}
